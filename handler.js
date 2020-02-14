827bb95860896714e3c9dc7cc406a07ac0262c3f
ashbtivish <ashbtivish@gmail.com>
2020-02-14 11:40:22 +0530

    Create handler.js

---
+++ handler.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports.helloWorld = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Ashish ! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
